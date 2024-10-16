515f9b3e43a446738b02f5a04835baad1ca94207
nzsombor04 <nzsombor04@gmail.hu>
2024-10-16 13:19:07 +0200

    XML file beolvasasa

--- HSZF_FelevesFeladat/Program.cs
+++ HSZF_FelevesFeladat/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace HSZF_FelevesFeladat
+﻿using System.Xml.Linq;
+
+namespace HSZF_FelevesFeladat
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+        }
+
+        public void XmlLoad()
+        {
+            // XML file beolvasas
+            string filePath = "employees-departments.xml";
+            XDocument xmlDoc = XDocument.Load(filePath);
         }
     }
 }
