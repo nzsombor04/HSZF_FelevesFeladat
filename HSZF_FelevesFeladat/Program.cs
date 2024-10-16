22ec7258a7e25b6605c2a6f4ed659060b315fb9a
nzsombor04 <nzsombor04@gmail.hu>
2024-10-16 13:26:40 +0200

    eur huf atvaltas

--- HSZF_FelevesFeladat/Program.cs
+++ HSZF_FelevesFeladat/Program.cs
@@ -18,28 +18,47 @@ namespace HSZF_FelevesFeladat
 
             //Minden alkalmazott feldolgozasa
             var employees = xmlDoc.Descendants("Employee")
-                .Select(e => new Employee
+                .Select(e =>
                 {
-                    EmployeeId = int.Parse(e.Attribute("employeeid").Value),
-                    Name = e.Element("Name").Value,
-                    BirthYear = int.Parse(e.Element("BirthYear").Value),
-                    StartYear = int.Parse(e.Element("StartYear").Value),
-                    CompletedProjects = int.Parse(e.Element("CompletedProjects").Value),
-                    Active = bool.Parse(e.Element("Active").Value),
-                    Retired = bool.Parse(e.Element("Retired").Value),
-                    Email = e.Element("Email").Value,
-                    Phone = e.Element("Phone").Value,
-                    Job = e.Element("Job").Value,
-                    Level = e.Element("Level").Value,
-                    Salary = int.Parse(e.Element("Salary").Value),
-                    Commission = int.Parse(e.Element("Commission").Value),
-                    Departments = e.Element("Departments").Elements("Department")
-                        .Select(d => new Department
+                    int commission = 0;
+                    if (e.Element("Commission") != null)
+                    {
+                        var commissionElement = e.Element("Commission");
+                        var currency = commissionElement.Element("currency")?.Value;
+                        if (currency == "eur")
                         {
-                            DepartmentName = d.Element("Name").Value,
-                            DepartmentCode = d.Element("DepartmentCode").Value,
-                            HeadOfDepartment = d.Element("HeadOfDepartment").Value
-                        }).ToList()
+                            commission = int.Parse(commissionElement.Value) * 400;
+                        }
+                        else
+                        {
+                            commission = int.Parse(commissionElement.Value);
+                        }
+                    }
+
+
+                    return new Employee
+                    {
+                        EmployeeId = int.Parse(e.Attribute("employeeid").Value),
+                        Name = e.Element("Name").Value,
+                        BirthYear = int.Parse(e.Element("BirthYear").Value),
+                        StartYear = int.Parse(e.Element("StartYear").Value),
+                        CompletedProjects = int.Parse(e.Element("CompletedProjects").Value),
+                        Active = bool.Parse(e.Element("Active").Value),
+                        Retired = bool.Parse(e.Element("Retired").Value),
+                        Email = e.Element("Email").Value,
+                        Phone = e.Element("Phone").Value,
+                        Job = e.Element("Job").Value,
+                        Level = e.Element("Level").Value,
+                        Salary = int.Parse(e.Element("Salary").Value),
+                        Commission = int.Parse(e.Element("Commission").Value),
+                        Departments = e.Element("Departments").Elements("Department")
+                            .Select(d => new Department
+                            {
+                                DepartmentName = d.Element("Name").Value,
+                                DepartmentCode = d.Element("DepartmentCode").Value,
+                                HeadOfDepartment = d.Element("HeadOfDepartment").Value
+                            }).ToList()
+                    };
                 }).ToList();
 
             return employees;
