5f965f54504b1ba76efcc930db26f3853d4e7f0e
nzsombor04 <nzsombor04@gmail.hu>
2024-10-16 13:17:01 +0200

    Add project files.

---
+++ HSZF_FelevesFeladat/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HSZF_FelevesFeladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
