a2b16e6dad99b868f4b10995be812d24d4572d7b
nzsombor04 <nzsombor04@gmail.hu>
2024-10-16 13:22:14 +0200

    Department es Employee osztaly letrehozva

---
+++ HSZF_FelevesFeladat/Model/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSZF_FelevesFeladat.Model
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+        public string DepartmentCode { get; set; }
+        public string HeadOfDepartment { get; set; }
+        public virtual Employee Employee { get; set; }
+    }
+}
---
+++ HSZF_FelevesFeladat/Model/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSZF_FelevesFeladat.Model
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public int BirthYear { get; set; }
+        public int StartYear { get; set; }
+        public int CompletedProjects { get; set; }
+        public bool Active { get; set; }
+        public bool Retired { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Job { get; set; }
+        public string Level { get; set; }
+        public decimal Salary { get; set; }
+        public decimal Commission { get; set; }
+        public virtual ICollection<Department> Departments { get; set; }
+    }
+}
