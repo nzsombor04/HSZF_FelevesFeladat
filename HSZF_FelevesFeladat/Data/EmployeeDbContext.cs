e348a03b5cd99e143c8f9d4767d55067471fe437
nzsombor04 <nzsombor04@gmail.hu>
2024-10-16 13:36:25 +0200

    DbContext osztaly definialasa

---
+++ HSZF_FelevesFeladat/Data/EmployeeDbContext.cs
@@ -0,0 +1,17 @@
+﻿using HSZF_FelevesFeladat.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSZF_FelevesFeladat.Data
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        
+    }
+}
